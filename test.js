f4e0466fbac36b76819606aa771efe93612d7378
Luis Couto <hello@luiscouto.pt>
2022-02-03 16:40:40 +0000

    add lmultuply operation

--- test.js
+++ test.js
@@ -3,3 +3,5 @@ const subtract = (a, b) => a - b
 const multiply = (a, b) => a * b
 const division = (a, b) => a / b
 const mod = (a, b) => a % b
+
+const lmultiply = (a, b) => [...Array(a)].reduce((acc) => sum(acc, b), 0)
